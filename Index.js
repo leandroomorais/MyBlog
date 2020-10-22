39787891305097194f53f422fb67344d750b8363
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2020-10-22 18:17:26 -0300

    Atualizar index.js

--- Index.js
+++ Index.js
@@ -2,8 +2,18 @@
 const express = require("express");
 const app = express();
 
+//Importing the body-parser
+const bodyParser = require("body-parser");
+
 //Seting up the view engine
 app.set("view engine", "ejs");
+
+//Seting up the body-parser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Seting up the static files
+app.use(express.static("public"));
 
 app.listen(8080, () => {
     console.log("Servidor rodando")
