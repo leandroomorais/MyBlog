e9ad3ff49b300bc1010663cb160e83f224117199
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2020-10-23 15:27:38 -0300

    Criação do Controller Categories

---
+++ categories/CategoriesController.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/categories", (req, res) => {
+    res.send("Rota de categoria")
+});
+
+router.get("/admin/categories/new", (req, res) => {
+    res.send("Rota criar nova categoria")
+});
