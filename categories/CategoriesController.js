f792de7c410fe359f850c3fe834f9c042f98ce8a
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2020-10-27 09:58:48 -0300

    Criação de rota para deletar Categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -6,26 +6,50 @@ const Category = require("./Category");
 //Importing Slugify
 const slugify = require("slugify");
 
-router.get("/categories", (req, res) => {
-    res.send("Rota de categoria")
-});
-
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new.ejs");
 });
 
 router.post("/categories/save", (req, res) => {
     var title = req.body.title;
-    if(title != undefined){
+    if (title != undefined) {
         Category.create({
             title: title,
             slug: slugify(title),
         }).then(() => {
             res.redirect("/");
         })
-    }else{
+    } else {
         res.redirect("/admin/categories/new");
     }
-})
+});
 
+router.get("/admin/categories", (req, res) => {
+
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", { categories: categories });
+    });
+});
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories")
+            })
+
+        } else {
+            res.redirect("/admin/categories");
+        }
+
+    } else {
+        res.redirect("/admin/categories")
+    }
+})
 module.exports = router;
