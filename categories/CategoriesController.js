74968448688ab6a4eb29dc1f7bf8d0e3f0bcdfd9
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2020-10-30 14:46:33 -0300

    Criação de formulário e rota para edição do Model Category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -17,12 +17,25 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title),
         }).then(() => {
-            res.redirect("/");
+            res.redirect("/admin/categories");
         })
     } else {
         res.redirect("/admin/categories/new");
     }
 });
+
+router.post("/categories/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update({ title: title, slug: slugify(title) }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    });
+})
 
 router.get("/admin/categories", (req, res) => {
 
@@ -51,5 +64,19 @@ router.post("/categories/delete", (req, res) => {
     } else {
         res.redirect("/admin/categories")
     }
-})
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/edit", { category: category });
+        } else {
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    })
+
+});
 module.exports = router;
