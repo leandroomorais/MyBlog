113f1cabd4684416b18f96e65fb214bb66d7b2f5
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2020-11-01 15:55:42 -0300

    Criação de rota para deletar artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -19,6 +19,19 @@ router.get("/admin/articles/new", (req, res) => {
 
 });
 
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update({ title: title, slug: slugify(title) }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    });
+})
+
 router.post("/articles/save", (req, res) => {
     var title = req.body.title;
     var body = req.body.body;
@@ -32,6 +45,41 @@ router.post("/articles/save", (req, res) => {
     }).then(() => {
         res.redirect("/admin/articles");
     })
-})
+});
+
+router.post("/articles/delete", (req, res) => {
+    var id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles")
+            })
+
+        } else {
+            res.redirect("/admin/articles");
+        }
+
+    } else {
+        res.redirect("/admin/articles")
+    }
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if (article != undefined) {
+            res.render("admin/articles/edit", { article: article });
+        } else {
+            res.redirect("/admin/articles");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/articles");
+    })
+
+});
 
 module.exports = router;
