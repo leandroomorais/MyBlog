63e7b9d1e7e4f8ebcf25434a2b1541d22523448c
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2020-11-02 08:41:32 -0300

    Criação de filtro de artigos por categoria

--- Index.js
+++ Index.js
@@ -54,7 +54,7 @@ app.get("/", (req, res) => {
 
     })
 
-})
+});
 
 app.get("/:slug", (req, res) => {
     var slug = req.params.slug;
@@ -73,6 +73,27 @@ app.get("/:slug", (req, res) => {
 
     }).catch(erro => {
         res.redirect("/");
+    })
+});
+
+app.get("/category/:slug", (req, res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug,
+        },
+        include: [{ model: Article }]
+    }).then(category => {
+        if (category != undefined) {
+            Category.findAll().then(categories => {
+                res.render("index", { articles: category.articles, categories: categories });
+            })
+
+        } else {
+            res.redirect("/");
+        }
+    }).catch(erro => {
+        res.redirect("/")
     })
 })
 
